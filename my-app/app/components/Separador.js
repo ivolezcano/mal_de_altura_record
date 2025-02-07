7c3c78140e577595bda3f72342ce0b0d86c1a923
Ivan Lezcano <lezcamati@gmail.com>
2025-02-07 15:02:39 -0300

    Detalles de responsividad

--- my-app/app/components/Separador.js
+++ my-app/app/components/Separador.js
@@ -2,13 +2,6 @@ import styles from '@/app/styles/Separador.module.css'
 
 const Separador = () =>{
     return(
-        <div className={styles.blanco}>
-        <div className={styles.separador1}>
-
-        </div>
-        <div className={styles.separador1}>
-
-        </div>
         <div className={styles.separador}>
             <div className={styles.banner}>
                 <h3 className='estilo'>¿CANSADO DE NO ENCONTRAR LA <br/>MÚSICA QUE SE AJUSTA A TÍ?</h3>
@@ -16,7 +9,6 @@ const Separador = () =>{
             <div className={styles.banner2}>
                 <h3 className='estilo'>NUESTROS SERVICIOS ESTÁN PENSADOS PARA <br/>AYUDARTE, ECHALES UN OJO DEBAJO</h3>
             </div>
-        </div>
         </div>
     )
 }
